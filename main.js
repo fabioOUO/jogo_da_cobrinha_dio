417e805ea10499709b5aec5d677d50f896a7d386
Fabio Junior Barbosa <fabiojbarbosa1@gmail.com>
2021-06-18 15:45:29 -0300

    verificar se cobra esta saindo da tela

--- main.js
+++ main.js
@@ -7,16 +7,15 @@ let canvas = document.getElementById("game");
 //cria context
 let context = canvas.getContext("2d");
 
+//pega tecla clicada e envia para a funcao getDirection
+document.addEventListener("keydown", getDirection);
+
 //inicializção de variaveis
 let box = 32;
 let game = null;
 let direction = "right";
 let time = 200;
 let snake = [];
-snake[0] = {
-    x: 2 * box,
-    y: 2 * box
-}
 level.innerHTML = 0;
 lengthSnake.innerHTML = 0;
 
@@ -36,10 +35,48 @@ function createSnake(){
     }
 }
 
+function getDirection(event){
+    //define a direção aparti da tecla clicada
+    let key = event.keyCode;
+    if(key == 37 && direction != "right") direction = "left";
+    if(key == 38 && direction != "up") direction = "down";
+    if(key == 39 && direction != "left") direction = "right";
+    if(key == 40 && direction != "down") direction = "up";
+}
+
+function startVars(){
+    //limpa contador de tempo
+    game!=null?clearInterval(game):null;
+    
+    //inicializa variaveis
+    game = null;
+    direction = "right";
+    time = 200;
+    level.innerHTML = 0;
+    lengthSnake.innerHTML = 0;
+    snake = [];
+    snake[0] = {
+        x: 2 * box,
+        y: 2 * box
+    } 
+}
+
+function checkEndOfScreen(){
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 * box && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "up") snake[0].y = 0;
+    if(snake[0].y < 0 * box && direction == "down") snake[0].y = 16 * box;
+}
+
 function start(){
+    startVars();
     game = setInterval(()=>{
+        //verifica fim de tela
+        checkEndOfScreen()
+        
         //redenriza o fundo
         createBG();
+        
         //renderiza a cobra
         createSnake();
     
